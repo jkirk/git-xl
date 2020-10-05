30c9495743f033cd41147fc5ded3d8feafc8094b
Darshaka Pathirana <dpat@syn-net.org>
2020-10-06 01:19:22 +0200

    [WIP] Add Linux support

---
+++ scripts/linux/git-xl-version-info.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(0, 0, 0, 0),
+    prodvers=(0, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'Zoomer Analytics LLC'),
+        StringStruct(u'FileDescription', u'Git XL'),
+        StringStruct(u'FileVersion', u'0.0.0'),
+        StringStruct(u'InternalName', u'git-xl'),
+        StringStruct(u'LegalCopyright', u'Zoomer Analytics LLC'),
+        StringStruct(u'OriginalFilename', u'git-xl'),
+        StringStruct(u'ProductName', u'Git XL'),
+        StringStruct(u'ProductVersion', u'0.0.0')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
